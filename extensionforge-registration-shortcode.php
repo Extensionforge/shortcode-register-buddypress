7e89fb5a58fcf4f9e36eaf7ab6ae598c064debc3
DESKTOP-K4P3EBG\peter <peter.mertzlin@gmail.com>
2023-01-09 15:41:27 +0100

    fixed bugs - double password - password strength - AGB link

--- extensionforge-registration-shortcode.php
+++ extensionforge-registration-shortcode.php
@@ -39,6 +39,147 @@ define( 'EXTENSIONFORGE_REGISTRATION_SHORTCODE_VERSION', '1.0.0' );
 
 define('EXTENSIONFORGE_REGISTRATION_INCLUDE_URL', plugin_dir_url(__FILE__).'includes/');
 
+add_action( 'template_redirect', 'redirect_to_extensionforge_registration' );
+function redirect_to_extensionforge_registration() {
+    if ( is_page('registrierung') && ! is_user_logged_in() ) {
+      wp_redirect( 'https://wpclub2.webtyphoon.productions/registration/', 301 ); 
+      exit;
+    }
+}
+
+
+function bpdev_ua_check_username() {
+    
+    if ( ! empty( $_POST["user_name"] ) ) {  $user_name = $_POST["user_name"]; } else {  $user_name ="";}
+    if ( ! empty( $_POST["user_email"] ) ) {  $user_email = $_POST["user_email"]; } else {  $user_email ="";}
+    
+    $errors = extf_bpdev_validate_username($user_name,$user_email);
+
+
+    if ( count( $errors->get_error_messages() ) <1) {
+                $msg = array( "code" => "success", "message" => __( "Dieser Benutzername ist verfügbar.", "buddypress" ) );
+            } else {
+
+        $error_msg = "";
+         $messages = $errors->get_error_messages();
+         foreach ($messages as $message) {
+            $error_msg.= $message."<br />";
+        }
+            $msg = array( "code" => "error", "message" => __( $error_msg, "buddypress" ) );
+        }
+   
+    wp_send_json( $msg );
+}
+
+
+add_action('wp_ajax_nopriv_bpdev_ua_check_username', 'bpdev_ua_check_username');
+add_action('wp_ajax_bpdev_ua_check_username', 'bpdev_ua_check_username');
+
+/* helper function to check the username is valid or not, thanks to @apeatling, taken from bp-core/bp-core-signup.php and modified for chacking only the username
+ * original:bp_core_validate_user_signup()
+ *
+ **/
+
+function extf_bpdev_validate_username( $user_name, $user_email ) {
+    
+    global $wpdb;
+    $table_users = $wpdb->prefix . 'users';
+    $table_signups = $wpdb->prefix . 'signups';
+    $errors = new WP_Error();
+    $maybe = array();
+
+        //check for the username in the signups table
+       
+        $usernameerror = false;
+       
+        // check email
+        $mailerror = false;
+
+        // check waiting for activation
+
+        $emailtesterx = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $table_signups where user_email = '%s'", $user_email ) );
+            
+        if ( ! empty( $emailtesterx ) && ($mailerror==false)) {
+             $errors->add( 'user_name', __( 'Diese Emailadresse ist bereits registriert, aber noch nicht aktiviert. Bitte prüfen Sie Ihr Email-Postfach.', 'buddypress' ) );  $mailerror = true;
+        }
+
+        $emailtester = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $table_users where user_email = '%s'", $user_email ) );
+            
+        if ( ! empty( $emailtester ) && ($mailerror==false) ) {
+             $errors->add( 'user_name', __( 'Diese Emailadresse ist bereits registriert.', 'buddypress' ) );  $mailerror = true;
+        }
+
+        
+
+        $user = $wpdb->get_row($wpdb->prepare("SELECT * from $table_signups where user_login = '%s'", $user_name));
+
+        if ( ! empty( $user ) && ($mailerror==false)) {
+             $errors->add( 'user_name', __( 'Dieser Benutzername ist bereits vergeben. Bitte wählen Sie einen anderen.', 'buddypress' ) ); $usernameerror = true;
+        }
+
+        $userx = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $table_users where user_login = '%s'", $user_name ) );
+            
+        if ( ! empty( $userx ) && ($usernameerror==false) && ($mailerror==false)) {
+             $errors->add( 'user_name', __( 'Dieser Benutzername ist bereits vergeben. Bitte wählen Sie einen anderen.', 'buddypress' ) ); 
+        }
+
+        if ( empty( $user_name ) ) {
+            // must not be empty.
+            $errors->add( 'user_name', __( 'Bitte verwenden Sie einen gültigen Benutzernamen.', 'buddypress' ) );
+        }
+
+        if ( function_exists( 'buddypress' ) ) {
+            $user_name = preg_replace( '/\s+/', '', $user_name );
+
+        }
+
+        // check blacklist.
+        $illegal_names = get_site_option( 'illegal_names' );
+        if ( in_array( $user_name, (array) $illegal_names ) ) {
+            $errors->add( 'user_name', __( 'Dieser Benutzername ist nicht erlaubt.', 'buddypress' ) );
+        }
+
+        // see if passed validity check.
+        if ( ! validate_username( $user_name ) ) {
+            $errors->add( 'user_name', __( 'Der Benutzername darf nur Kleinbuchstaben und Zahlen enthalten.', 'buddypress' ) );
+        }
+
+        if ( strlen( $user_name ) < 4 ) {
+            $errors->add( 'user_name', __( 'Der Benutzername muss mindestens 4 Zeichen lang sein.', 'buddypress' ) );
+        } elseif ( mb_strlen( $user_name ) > 20 ) {
+            $errors->add( 'user_login_too_long', __( 'Der Benutzername darf nicht länger als 20 Zeichen sein.', 'buddypress' ) );
+        }
+
+        if ( strpos( ' ' . $user_name, '_' ) != false ) {
+            $errors->add( 'user_name', __( 'Sorry, usernames may not contain the character "_"!', 'buddypress' ) );
+        }
+
+        /* Is the user_name all numeric? */
+        $match = array();
+        preg_match( '/[0-9]*/', $user_name, $match );
+
+        if ( $match[0] == $user_name ) {
+            $errors->add( 'user_name', __( 'Sorry, usernames must have letters too!', 'buddypress' ) );
+        }
+
+        /**
+         * Filters the list of blacklisted usernames.
+         *
+         * @param array $usernames Array of blacklisted usernames.
+         */
+        $illegal_logins = (array) apply_filters( 'illegal_user_logins', array() );
+
+        if ( in_array( strtolower( $user_name ), array_map( 'strtolower', $illegal_logins ) ) ) {
+            $errors->add( 'invalid_username', __( 'Sorry, that username is not allowed.', 'buddypress' ) );
+        }
+
+    
+    return $errors;
+}
+
+
+
+
 
 //add front end css and js
 function srf_slider_trigger(){
@@ -64,14 +205,14 @@ function extensionforge_registration_shortcode( $atts ) {
     
     //if looged in rediret to home page
     if ( is_user_logged_in() ) { 
-       // wp_redirect( get_option('home') );// redirect to home page
-       // exit;
+        wp_redirect( get_option('home') );// redirect to home page
+        exit;
     }
 
     if(isset( $_POST['com_submit'])){
         $prenom=sanitize_text_field( $_REQUEST['prenom'] );
-        $firstname=sanitize_text_field( $_REQUEST['com_firstname'] );
-        $lastname=sanitize_text_field( $_REQUEST['com_lastname']);
+        $firstname=$_REQUEST['com_firstname'] ;
+        $lastname=$_REQUEST['com_lastname'];
         $username = sanitize_text_field(  $_REQUEST['com_username'] );
         $email = sanitize_text_field(  $_REQUEST['com_email']  );
         $kdnr = sanitize_text_field( $_REQUEST['com_kdnr']);
@@ -79,11 +220,23 @@ function extensionforge_registration_shortcode( $atts ) {
         $password = sanitize_text_field( $_REQUEST['com_password']);
         if(isset($_REQUEST['nldropdown'])){$interests = $_REQUEST['nldropdown'];  } 
 
+        // check if username ok?
+
+        $errors = extf_bpdev_validate_username($username,$email);
+
+        //var_dump($errors);
+        //echo "ERRORS: ".count( $errors->get_error_messages())."<br />";
+       
+        if ( count( $errors->get_error_messages() ) <1) {
 
         $table_name = $wpdb->prefix . 'signups';
+        $user_meta = $wpdb->prefix . 'usermeta';
         $jetzt = wp_date('Y-m-d H:i:s');
         
         $token = wp_generate_password( 32, false );
+
+        $nicename = strtolower($firstname."-".$lastname);
+        $displayname = $firstname." ".$lastname;
 
          $default_newuser = array(
         'user_pass' =>  $password,
@@ -91,14 +244,17 @@ function extensionforge_registration_shortcode( $atts ) {
         'user_email' => $email,
         'first_name' => $firstname,
         'last_name' => $lastname,
+        'user_nicename' => $nicename,
+        'display_name' => $displayname,
         'user_registered' => $jetzt
         );
+
 
         if(count($interests)>0){
         $sinterests = implode("," , $interests); }
 
-        $user_id = wp_insert_user($default_newuser);
-        $status = $user_id;
+        $status = wp_insert_user($default_newuser);
+        $user_id = $status;
         $fieldids = "10,13,1,2,15,16";
 
         $smeta = array("field_10" => $prenom, "field_10_visibility" => "adminsonly",
@@ -128,8 +284,8 @@ function extensionforge_registration_shortcode( $atts ) {
 
     if ( $user_id && !is_wp_error( $user_id ) ) {
         $wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->users} SET user_status = 2 WHERE ID = %d", $user_id ) );
+        
 
-       
         delete_user_option( $user_id, 'capabilities' );
         delete_user_option( $user_id, 'user_level'   );
 
@@ -154,29 +310,31 @@ function extensionforge_registration_shortcode( $atts ) {
        //wp_mail( $email, 'SUBJECT', 'Activation link : ' . $activation_link );
         bp_core_signup_send_validation_email($user_id,$email,$token,$username);  
 
-        $success ="";
-        $error_msg="";
-       
-        if (is_wp_error($status))  {
-             $error_msg = __('Username or Email already registered. Please try another one.',""); 
-        } 
-        else{
-            $user_id=$status;
-            
-            
+        
             $success= __('<div class="vnr_register_success">Vielen Dank für Ihre kostenlose Registrierung im Computerwissen-Club.<br />Ihre Anmeldedaten sind bei uns angekommen.<br /><br />In Kürze erhalten Sie eine E-Mail, in der wir Ihre Angaben zur Registrierung noch einmal für Sie zusammengefasst haben.<br />Der Betreff der E-Mail lautet:<br /> >>> Wichtig: Bitte noch bestätigen: Ihre Registrierung im Computerwissen-Club <<< <br /><br />Damit wir sicherstellen können, dass Sie der Inhaber der angegebenen Adresse sind, müssen Sie diese bitte noch bestätigen mit einem Klick auf den Bestätigungslink.<br /><br />Dann ist Ihre Anmeldung im Club abgeschlossen.<br /><br />Wir freuen uns, Sie bald als neues Club-Mitglied begrüßen zu dürfen.<br />Ihr Team vom Computerwissen-Club</div>',""); 
-            
         }  
+    } else {
+         $error_msg = "";
+         $messages = $errors->get_error_messages();
+         foreach ($messages as $message) {
+            $error_msg.= $message."<br />";
+         }
+        // error! no valid username or email or something
+      
+      
     }
 }
 
-$output ='<div id="buddypress">
+$output ='<script>function checkextfformchecker() { test = document.getElementById("pass-strength-result").innerHTML; if (test=="Stark"){
+    document.getElementById("extf_pwhinweis").style.display = "none"; return true;} else {document.getElementById("extf_pwhinweis").style.display = "block"; return false;}
+
+    } function extf_copyvalue(){ document.getElementById("password2").value=document.getElementById("password1").value;checkextfformchecker();}</script><div id="buddypress">
     <div class="page" id="register-page">
         <div class="register-section" id="basic-details-section">';
-            if(isset($error_msg)) { $output .='<div class="error">'.$error_msg.'</div>'; }  
+            if(isset($error_msg)) { $output .='<div style="display:block!important;width:100%; text-align:left;" class="vnr_useremailerror">Eingabe-Fehler! Bitte ändern Sie Ihre Eingabe!</div>'; }  
             if(isset($success)) { $output .='<div class="success">'.$success.'</div>'; }  else {
         
-$output .='<form action="" name="signup_form" id="signup_form" class="standard-form" method="post" enctype="multipart/form-data">
+$output .='<form action="" name="signup_form" id="signup_form" onsubmit="return checkextfformchecker()" class="standard-form" method="post" enctype="multipart/form-data">
                 <label>Anrede</label> 
                 <select id="prenom" name="prenom" class="input">
                     <option value="Keine">auswählen</option>
@@ -189,19 +347,22 @@ $output .='<form action="" name="signup_form" id="signup_form" class="standard-f
                 <label>Nachname</label>  
                 <input id="com_lastname" name="com_lastname" type="text" class="input" value='.$lastname.' >
                 <label>Benutzername *</label> 
-                <input id="com_username" name="com_username" pattern="[a-zA-Z0-9äÄöÖüÜß]*" type="text" class="input" required value='.$username.' >
-                <div class="vnr_only_numbersletters">(Nur Buchstaben und Zahlen)</div>
+                <input id="com_username" name="com_username" pattern="[a-z0-9äöüß]*" type="text" class="input" required value='.$username.' >
+                <div class="vnr_only_numbersletters">(Nur kleine Buchstaben und Zahlen)</div>
+                <div id="vnr_useremailerror"   ';
+    if(isset($error_msg)) { $output .=' style="display:block; width:100%; text-align:left;">'.$error_msg; }  else { $output .='>'; }
+                $output .='</div>
                 <label>E-Mail *</label>
                 <input id="com_email" name="com_email" type="email" class="input" required value='.$email.' >
                 <div class="vnr_emailbestaetigung">(An diese E-Mail schicken wir Ihnen die Anmeldebestätigung)</div>
          
                 <label>Passwort *</label>
-                <input id="password1" name="com_password" type="password" required class="input" />
-            
+                <input id="password1" name="com_password" type="password" oninput="extf_copyvalue()" required class="input" />
+                <div id="extf_pwhinweis" class="extf_passworthinweisstark">Bitte verwenden Sie ein starkes Passwort!</div>
                 <div id="pass-strength-result" style="width:208px;"></div>
         
-                <label>Passwort Wiederholung</label>
-                <input id="password2" name="c_password" type="password" class="input" />
+              
+                <input id="password2" name="c_password" type="password"  style="display:none" class="input" />
                 <div class="vnr_pwanforderung">(Min. 8 Zeichen, bitte Groß- & Kleinbuchstaben, Zahlen und Sonderzeichen verwenden)
 Sonderzeichen Bsp.: ( ) = < > ? _ ! @ # $ % ^ & * </div>
                 <label>Kundennummer</label>
@@ -250,8 +411,7 @@ Sonderzeichen Bsp.: ( ) = < > ? _ ! @ # $ % ^ & * </div>
                     <label for="agb" class="control-label">AGB *</label> 
                     <div>
                         <label class="checkbox-inline">
-                            <input type="checkbox" name="agb" value="ja" required>
-                            <a href="https://club.computerwissen.de/agb" target="_blank" class="agbcheckboxlink">Ja, ich habe die AGBs gelesen und akzeptiert.</a>
+                            <input type="checkbox" name="agb" value="ja" required>Ja, ich habe die&nbsp;<a href="https://club.computerwissen.de/agb" target="_blank" class="agbcheckboxlink">AGB</a>&nbsp;gelesen und akzeptiert.
                         </label>
                     </div>
                 </div>
@@ -309,6 +469,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-extensionforge-registratio
  *
  * @since    1.0.0
  */
+
+
+
+
 function run_extensionforge_registration_shortcode() {
 
     $plugin = new Extensionforge_Registration_Shortcode();
--- public/js/extensionforge-registration-shortcode-public.js
+++ public/js/extensionforge-registration-shortcode-public.js
@@ -1,32 +1,89 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$( document ).ready( function () {
+
+	  var j = jQuery;
+
+	$( "input#com_username" ).wrap( "<div id='username_checker'></div> " );
+	$( "#username_checker" ).append( "<span class='loading' style='display:none'></span>" )
+	$( "#username_checker" ).append( "<span id='name-info'></span> " );
+	
+	$( "input#com_username" ).bind( "blur", function () {
+
+		$( "#username_checker #name-info" ).empty();
+		//show loading icon
+		$( "#username_checker .loading" ).css( { display: 'block' } );
+
+		var user_name = j( "input#com_username" ).val();
+
+		var user_email = j( "input#com_email" ).val();
+
+
+
+		user_name = user_name.toLowerCase();
+		//string = user_name.replace(/[^a-zA-Z ]/g, "");
+		$("input#com_username").val(user_name);
+
+		   $.ajax({
+            type: 'POST',
+            url: ajaxurl,
+            data: {
+               action: 'bpdev_ua_check_username',
+               user_name: user_name,
+               user_email: user_email
+            },  
+           success: function(resp) {
+                      if( resp && resp.code != undefined && resp.code == 'success' ) {
+						//alert(resp.message);
+						show_message(resp.message,0);
+						} else {
+						//alert(resp.message);
+						show_message(resp.message,1);
+						}
+                      },
+                      error: function() {
+                        alert('There was some error performing!');
+                      }
+        });
+	} );
+
+
+
+function removeSpecialCharacter(s)
+{
+	for (let i = 0; i < s.length; i++)
+		{
+
+			// Finding the character whose
+			// ASCII value fall under this
+			// range
+			if (s[i] < 'A' || s[i] > 'Z' &&
+					s[i] < 'a' || s[i] > 'z')
+			{
+				
+				// erase function to erase
+				// the character
+				s = s.substring(0, i) + s.substring(i + 1);
+				i--;
+			}
+		}
+		return s;
+}
+
+	
+	function show_message( msg, is_error )	{//hide ajax loader
+		
+		j( "#username_checker #name-info" ).removeClass();
+		j( "#username_checker .loading" ).css( { display: 'none' } );
+		j( "#username_checker #name-info" ).empty().html( msg );
+		
+		if ( is_error ) {
+			j( "#username_checker #name-info" ).addClass( "error" );
+		} else {
+			j( "#username_checker #name-info" ).addClass( "available" );
+		}
+	}
+} );
 
 })( jQuery );
